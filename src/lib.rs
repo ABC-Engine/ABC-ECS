d56d23b30b22f64a9ae4029391bbe6eab394705e
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2023-11-27 17:27:10 -0600

    Outline some basic data structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+struct GameEngine {
+    entities: Vec<Entity>,
+    components: Vec<Vec<Box<dyn Component>>>, // where components[entity_id][component_id (which shouldn't need to indexed into)]
+    systems: Vec<Box<dyn System>>,
+}
+
+impl GameEngine {
+    pub fn new() -> Self {
+        GameEngine {
+            entities: vec![],
+            components: vec![],
+            systems: vec![],
+        }
+    }
+
+    pub fn get_components(&self, entity: Entity) -> &Vec<Box<dyn Component>> {
+        self.components
+            .get(entity.entity_id)
+            .expect("Entity ID does not exist, was the Entity ID edited?")
+    }
+
+    // TODO: add_entity
+    // how should the arguments to this function be handled?
+}
+
+pub trait Component {}
+
+// The Entity will just be an ID that can be
+// indexed into arrays of components for now...
+pub struct Entity {
+    entity_id: usize,
+}
+
+/// Systems access and change components on objects
+pub trait System {
+    fn run(&self, entities: &mut Vec<Entity>);
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
