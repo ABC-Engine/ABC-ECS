4758f5d9ce3a8d0db2be8633cbb22f2b3f2d5acc
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2023-12-07 10:46:41 -0600

    Improve testing and docs

--- src/lib.rs
+++ src/lib.rs
@@ -19,21 +19,50 @@ pub trait Components: 'static {
 /// This macro is used to get a variable ammount of components from an entity
 /// It returns a tuple of references to the components
 /// ```rust
-/// let (position, velocity) = get_components!(engine.entities_and_components, entity, Position, Velocity);
-/// println!("Position: {}, {}", position.x, position.y);
-/// println!("Velocity: {}, {}", velocity.x, velocity.y);
+/// use ABC_ECS::{get_components, GameEngine, Component};
+///
+/// struct Position {
+///     x: f32,
+///     y: f32,
+/// }
+///
+/// impl Component for Position {}
+///
+/// struct Velocity {
+///     x: f32,
+///     y: f32,
+/// }
+///
+/// impl Component for Velocity {}
+///
+///
+/// fn main() {
+///     let mut engine = GameEngine::new();
+///     let entities_and_components = &mut engine.entities_and_components;
+///
+///     let entity = entities_and_components.add_entity();
+///
+///     entities_and_components.add_component_to(entity, Position { x: 0.0, y: 0.0 });
+///     entities_and_components.add_component_to(entity, Velocity { x: 1.0, y: 1.0 });
+///
+///     let (position, velocity) = get_components!(engine.entities_and_components, entity, Position, Velocity);
+///
+///     println!("Position: {}, {}", position.x, position.y);
+///     println!("Velocity: {}, {}", velocity.x, velocity.y);
+/// }
 /// ```
+#[macro_export]
 macro_rules! get_components {
     ($engine:expr, $entity:expr, $($component:ty),*) => {
         {
             let mut all_types = vec![];
             $(
-                all_types.push(any::TypeId::of::<$component>());
+                all_types.push(std::any::TypeId::of::<$component>());
             )*
 
             for i in 0..all_types.len() {
                 for j in i+1..all_types.len() {
-                    assert_ne!(all_types[i], all_types[j]);
+                    assert_ne!(all_types[i], all_types[j], "You cannot borrow the same component more than once!");
                 }
             }
 
@@ -54,25 +83,56 @@ macro_rules! get_components {
 ///
 /// It returns a tuple of references to the components
 /// ```rust
-/// let (position, velocity) = get_components_mut!(engine.entities_and_components, entity, Position, Velocity);
-/// position.x += velocity.x;
-/// position.y += velocity.y;
-/// println!("Position: {}, {}", position.x, position.y);
+/// use ABC_ECS::{get_components_mut, GameEngine, Component};
+///
+/// struct Position {
+///     x: f32,
+///     y: f32,
+/// }
+///
+/// impl Component for Position {}
+///
+/// struct Velocity {
+///     x: f32,
+///     y: f32,
+/// }
+///
+/// impl Component for Velocity {}
+///
+///
+/// fn main() {
+///     let mut engine = GameEngine::new();
+///     let entities_and_components = &mut engine.entities_and_components;
+///
+///     let entity = entities_and_components.add_entity();
+///
+///     entities_and_components.add_component_to(entity, Position { x: 0.0, y: 0.0 });
+///     entities_and_components.add_component_to(entity, Velocity { x: 1.0, y: 1.0 });
+///
+///     let (position, velocity) = get_components_mut!(engine.entities_and_components, entity, Position, Velocity);
+///
+///     position.x += velocity.x;
+///     position.y += velocity.y;
+///
+///     println!("Position: {}, {}", position.x, position.y);
+/// }
 /// ```
+///
 /// WARNING: This macro is not safe to use if you are borrowing the same component mutably more than once
 ///
 /// It will panic if you do this in a single call to the macro, but it will not panic if you do it in seperate calls
+#[macro_export]
 macro_rules! get_components_mut {
     ($engine:expr, $entity:expr, $($component:ty),*) => {
         {
             let mut all_types = vec![];
             $(
-                all_types.push(any::TypeId::of::<$component>());
+                all_types.push(std::any::TypeId::of::<$component>());
             )*
 
             for i in 0..all_types.len() {
                 for j in i+1..all_types.len() {
-                    assert_ne!(all_types[i], all_types[j]);
+                    assert_ne!(all_types[i], all_types[j], "You cannot borrow the same component mutably more than once!");
                 }
             }
 
@@ -97,18 +157,25 @@ impl EntitiesAndComponents {
         }
     }
 
+    /// Gets a reference to all the components on an entity
+    /// Returns an AnyMap, which can be used to get a reference to a component
+    /// This should rarely if ever be used
     pub fn get_components(&self, entity: Entity) -> &AnyMap {
         self.components
             .get(entity.entity_id)
             .expect("Entity ID does not exist, was the Entity ID edited?")
     }
 
+    /// Gets a mutable reference to the components on an entity
+    /// If the entity does not exist, it will panic
     pub fn get_components_mut(&mut self, entity: Entity) -> &mut AnyMap {
         self.components
             .get_mut(entity.entity_id)
             .expect("Entity ID does not exist, was the Entity ID edited?")
     }
 
+    /// Gets a reference to a component on an entity
+    /// If the component does not exist on the entity, it will return None
     pub fn try_get_component<T: 'static>(&self, entity: Entity) -> Option<&Box<T>> {
         self.components
             .get(entity.entity_id)
@@ -116,6 +183,8 @@ impl EntitiesAndComponents {
             .get::<Box<T>>()
     }
 
+    /// Gets a mutable reference to a component on an entity
+    /// If the component does not exist on the entity, it will return None
     pub fn try_get_component_mut<T: 'static>(&mut self, entity: Entity) -> Option<&mut Box<T>> {
         self.components
             .get_mut(entity.entity_id)
@@ -123,6 +192,8 @@ impl EntitiesAndComponents {
             .get_mut::<Box<T>>()
     }
 
+    /// Gets a reference to a component on an entity
+    /// If the component does not exist on the entity, it will panic
     pub fn get_component<T: 'static>(&self, entity: Entity) -> &T {
         self.components
             .get(entity.entity_id)
@@ -133,6 +204,8 @@ impl EntitiesAndComponents {
             )
     }
 
+    /// Gets a mutable reference to a component on an entity
+    /// If the component does not exist on the entity, it will panic
     pub fn get_component_mut<T: 'static>(&mut self, entity: Entity) -> &mut T {
         self.components
             .get_mut(entity.entity_id)
@@ -143,6 +216,8 @@ impl EntitiesAndComponents {
             )
     }
 
+    /// Adds an entity to the game engine
+    /// Returns the entity
     pub fn add_entity(&mut self) -> Entity {
         let entity_id = self.components.insert(AnyMap::new());
         self.entities.push(Entity { entity_id });
@@ -150,6 +225,8 @@ impl EntitiesAndComponents {
         Entity { entity_id }
     }
 
+    /// Adds a component to an entity
+    /// If the component already exists on the entity, it will be overwritten
     pub fn add_component_to<T: Component>(&mut self, entity: Entity, component: T) {
         let components = self
             .components
@@ -160,8 +237,8 @@ impl EntitiesAndComponents {
     }
 }
 
-struct GameEngine {
-    entities_and_components: EntitiesAndComponents,
+pub struct GameEngine {
+    pub entities_and_components: EntitiesAndComponents,
     systems: Vec<Box<dyn System>>,
 }
 
@@ -186,9 +263,7 @@ impl GameEngine {
     }
 }
 
-pub trait Component: 'static {
-    fn as_any(&self) -> &dyn any::Any;
-}
+pub trait Component: 'static {}
 
 // The Entity will just be an ID that can be
 // indexed into arrays of components for now...
@@ -211,28 +286,19 @@ mod tests {
         y: f32,
     }
 
-    impl Component for Position {
-        fn as_any(&self) -> &dyn any::Any {
-            self
-        }
-    }
+    impl Component for Position {}
 
     struct Velocity {
         x: f32,
         y: f32,
     }
 
-    impl Component for Velocity {
-        fn as_any(&self) -> &dyn any::Any {
-            self
-        }
-    }
+    impl Component for Velocity {}
 
     struct MovementSystem {}
 
     impl System for MovementSystem {
         fn run(&self, engine: &mut EntitiesAndComponents) {
-            println!("Running Movement System");
             for i in 0..engine.entities.len() {
                 let entity = engine.entities[i];
 
@@ -264,5 +330,32 @@ mod tests {
         for i in 0..5 {
             engine.run();
         }
+
+        // not sure how to test this yet...
+        // in the current state of the code, this causes a mutable borrow error
+        // not sure if that is a positive design choice or not
+
+        /*let position = entities_and_components.get_component::<Position>(entity);
+        assert_eq!(position.x, 5.0);
+        assert_eq!(position.y, 5.0);
+
+        let velocity = entities_and_components.get_component::<Velocity>(entity);
+        assert_eq!(velocity.x, 1.0);
+        assert_eq!(velocity.y, 1.0);*/
+    }
+
+    #[test]
+    fn test_overriding_components() {
+        let mut engine = GameEngine::new();
+        let entities_and_components = &mut engine.entities_and_components;
+
+        let entity = entities_and_components.add_entity();
+
+        entities_and_components.add_component_to(entity, Position { x: 0.0, y: 0.0 });
+        entities_and_components.add_component_to(entity, Position { x: 6.0, y: 1.0 });
+
+        let position = entities_and_components.get_component::<Position>(entity);
+        assert_eq!(position.x, 6.0);
+        assert_eq!(position.y, 1.0);
     }
 }
